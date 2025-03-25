a2cbd0d22841910514c863c90a8f2f42f3930d98
Michaela Chlubna <michaela.chlubna@seznam.cz>
2025-03-25 20:01:08 +0100

    vse v jedne funkci

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-// const selectPlan = (planNumber) => {
-//     document.querySelector("#plan2").classList.add('plan--selected')
-// }
-
-// selectPlan(plan2)
-
-// const removePlan = (planNumber) => {
-//     document.querySelectorAll('.plan').classList.remove('plan--selected')
-// }
-
-const removePlan = () => {
-    const plans = document.querySelectorAll('.plan');
-    plans.forEach(plan => plan.classList.remove('plan--selected'));
-}
-
-
 const selectPlan = (planNumber) => {
-document.querySelector(`#${planNumber}`).classList.add('plan--selected')
+    document.querySelectorAll('.plan').forEach(plan => plan.classList.remove('plan--selected'));
+    document.querySelector(`#${planNumber}`).classList.add('plan--selected')
 }
-
-removePlan()
 
 selectPlan("plan2")
+selectPlan("plan3")
