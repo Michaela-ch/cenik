e0d0358ad2c555ea96c5ed27efae285d48499ddc
Michaela Chlubna <michaela.chlubna@seznam.cz>
2025-03-25 20:11:26 +0100

    komplet hotovo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const selectPlan = (planNumber) => {
     document.querySelectorAll('.plan').forEach(plan => plan.classList.remove('plan--selected'));
-    document.querySelector(`#${planNumber}`).classList.add('plan--selected')
+    document.querySelector(`#plan${planNumber}`).classList.add('plan--selected')
 }
 
-selectPlan("plan2")
-selectPlan("plan3")
+selectPlan(2)
+selectPlan(3)
